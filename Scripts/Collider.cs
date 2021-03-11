c92e63e4b47207aed276c3d8b01982f0e78ed325
mohamedibrahim155 <63718196+mohamedibrahim155@users.noreply.github.com>
2021-03-11 17:14:11 +0530

    Add files via upload

---
+++ Scripts/Collider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collider : MonoBehaviour
+{
+
+    private void OnTriggerEnter(UnityEngine.Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+        // repositioning with collider
+        
+           Spawner.instance.RePostionWalls();
+        
+        }
+    }
+}
