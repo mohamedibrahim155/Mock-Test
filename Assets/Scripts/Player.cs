c985cae4327e75869d00472a6176402c299449af
mohamedibrahim155 <63718196+mohamedibrahim155@users.noreply.github.com>
2021-03-11 17:26:56 +0530

    Add files via upload

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    float horizontalAxis;
+   public float moveSpeed;
+    Transform raycastTransform;
+    LayerMask obstacle;
+    RaycastHit hitInfo;
+    float temp;
+   public Quaternion targetRotation;
+    public enum rotate  {forward,backward };
+    public rotate RotationDirection;
+    Vector3 rotateDir;
+    public Material red, blue, yellow,Green;
+    public GameObject ParticlePrefab;
+    public Transform particleSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalAxis = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward *moveSpeed*Time.deltaTime);
+
+        switch (RotationDirection)
+        {
+            case rotate.forward:
+              rotateDir=  Vector3.forward;
+                break;
+            case rotate.backward:
+              rotateDir=  Vector3.back;
+                break;
+            default:
+                break;
+        }
+       
+        Inputs();
+
+        // Checking angle and rotating the tile
+        if (hitInfo.collider!=null)
+        {
+            if (Quaternion.Angle(hitInfo.transform.parent.rotation, targetRotation) > 0.5f)
+            {
+
+                hitInfo.transform.parent.Rotate(rotateDir * 6f, Space.World);
+
+            }
+        }
+
+        if (transform.position.y<-4f)
+        {
+            SceneManager.LoadScene(2);
+        }
+                            
+      
+
+    }
+
+
+    void Inputs()
+    {
+        //If mouse clicked 
+        if (Input.GetMouseButtonDown(0))
+        {
+            // ray to identify which to rotate  and setting roation value in Z axis
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hitInfo, Mathf.Infinity))
+            {
+                temp = hitInfo.transform.parent.localEulerAngles.z + 90;   
+                targetRotation = Quaternion.Euler(hitInfo.transform.parent.rotation.x, hitInfo.transform.parent.rotation.y, temp);
+                RotationDirection = rotate.forward;
+                
+
+            }
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            // ray to identify which to rotate  and setting roation value in Z axis
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hitInfo, Mathf.Infinity))
+            {
+                temp = hitInfo.transform.parent.localEulerAngles.z - 90;
+                targetRotation = Quaternion.Euler(hitInfo.transform.parent.rotation.x, hitInfo.transform.parent.rotation.y, temp);
+                RotationDirection = rotate.backward;
+
+
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // checking if the player collides with wrong color and decreasing the score
+        if (collision.transform.GetComponent<MeshRenderer>().material.color!= this.GetComponent<MeshRenderer>().material.color)
+        {
+           
+            GameObject temp = Instantiate(ParticlePrefab, particleSpawn.position, Quaternion.identity);
+            Destroy(temp, 1f);
+            this.GetComponent<MeshRenderer>().material = collision.transform.GetComponent<MeshRenderer>().material;
+            Scoremanager.instance.score -= 100f;
+        }
+        else
+        {
+            Scoremanager.instance.score += 5f;
+        }
+      
+    }
+    
+}
---
+++ Assets/Scripts/Scoremanager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class Scoremanager : MonoBehaviour
+{
+    public TextMeshProUGUI ScoreText;
+  public  float score;
+    public static Scoremanager instance;
+    private void Awake()
+    {
+        if (instance==null)
+        {
+            instance = this;
+
+        }
+        else
+        {
+            Destroy(instance);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 800f;
+        ScoreText.text = score.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (score<=0)
+        {
+            SceneManager.LoadScene(2);
+        }
+        ScoreText.text = score.ToString();
+    }
+
+   
+}
